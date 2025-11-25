f0dea1e32903411ae4c296a997d35cb2419ffa44
Subramaniam, Rajalingam <RAJALINGAM.SUBRAMANIAM@OPTUM.COM>
2025-11-25 17:14:10 +0530

    Fix the issue
    Fix the issue

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,10 +1,10 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 
-import { HapticTab } from '../../components/haptic-tab';
-import { IconSymbol } from '../../components/ui/icon-symbol';
-import { Colors } from '../../constants/theme';
-import { useColorScheme } from '../../hooks/use-color-scheme';
+import { HapticTab } from '../../../components/haptic-tab';
+import { IconSymbol } from '../../../components/ui/icon-symbol';
+import { Colors } from '../../../constants/theme';
+import { useColorScheme } from '../../../hooks/use-color-scheme';
 import { useAuth } from '../context/AuthContext';
 import { Redirect } from 'expo-router';
 
