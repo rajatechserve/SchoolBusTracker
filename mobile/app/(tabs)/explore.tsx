14ff616d1439014369baa07b127b0e6fcf0fc3a8
Subramaniam, Rajalingam <RAJALINGAM.SUBRAMANIAM@OPTUM.COM>
2025-11-25 17:20:56 +0530

    Fix the chanegs
    Fix the chanegs

--- mobile/app/(tabs)/explore.tsx
+++ mobile/app/(tabs)/explore.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, StyleSheet, Text, TouchableOpacity, Alert } from 'react-native';
 import { useAuth } from '../context/AuthContext';
 import { useRouter } from 'expo-router';
-import theme from '../../constants/theme';
+import theme from '../constants/theme';
 
 export default function ExploreScreen() {
   const { user, logout } = useAuth();
